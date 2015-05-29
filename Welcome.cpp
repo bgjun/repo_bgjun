c25bfae19d8730a11fa2977c48c269624a08594b
bgjun <junbogon@gmail.com>
2015-05-29 23:21:20 +0900

    Update Welcome.cpp

--- Welcome.cpp
+++ Welcome.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main(void) {
     cout<<"Welcome!"<<endl;
-    cout<<"This is bgjun's cpp workspace!"<<endl;
+    cout<<"This is bgjun's c,cpp,java workspace!"<<endl;
     return 0;
 }
